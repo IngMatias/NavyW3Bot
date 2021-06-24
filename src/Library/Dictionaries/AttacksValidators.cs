820a227c38345087429c20c6a7763aa2f3a7f7f1
tomasmaurente <tommaurente@gmail.com>
2021-06-24 20:27:11 -0300

    Se instanciaron las variables de los diccionarios para que no se creen estas instancias cada ves que la clase sea llamada

--- src/Library/Dictionaries/AttacksValidators.cs
+++ src/Library/Dictionaries/AttacksValidators.cs
@@ -5,15 +5,31 @@ namespace Library
     public class AttacksValidators
     {
         private Dictionary<System.Type, IAttackValidator> _attackValidator;
+
+        // Vessels.
+        private IItem _antiAircraft = new AntiaircraftMissile();
+        private IItem _armor = new Armor();
+        private IItem _hackers = new Hackers();
+        private IItem _kong = new Kong();
+        private IItem _sateliteLock = new SateliteLock();
+
+        // AttackValidators.
+        private IAttackValidator _antiAircraftValidator = new AntiaircraftMissileAttackValidator();
+        private IAttackValidator _armorValidators = new ArmorAttackValidator();
+        private IAttackValidator _hackersValidators = new HackersAttackValidator();
+        private IAttackValidator _kongValidator = new KongAttackValidator();
+        private IAttackValidator _sateliteLock = new SateliteLockAttackValidator();
+
+
         public AttacksValidators()
         {
             this._attackValidator = new Dictionary<System.Type, IAttackValidator>
             {
-                {new AntiaircraftMissile().GetType(), new AntiaircraftMissileAttackValidator() },
-                {new Armor().GetType(), new ArmorAttackValidator() },
-                {new Hackers().GetType(), new HackersAttackValidator() },
-                {new Kong().GetType(), new KongAttackValidator() },
-                {new SateliteLock().GetType(), new SateliteLockAttackValidator() },
+                {_antiAircraft.GetType(), _antiAircraftValidator},
+                {_armor.GetType(), _armorValidators },
+                {_hackers.GetType(), _hackersValidators},
+                {_kong.GetType(), _kongValidator},
+                {_sateliteLock.GetType(), _sateliteLock},
             };
         }
         public IAttackValidator ValidatorOf(IItem item)
