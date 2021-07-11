02ab4723cac893344b803f109dffbf149a121a2b
IngMatias <67179252+IngMatias@users.noreply.github.com>
2021-07-11 12:50:00 -0300

    Update TelegramBot.cs

--- src/Library/9-Telegram/1-Bot/TelegramBot.cs
+++ src/Library/9-Telegram/1-Bot/TelegramBot.cs
@@ -6,7 +6,7 @@ namespace Library
 {
     public class TelegramBot
     {
-        private const string TELEGRAM_TOKEN = "1863846699:AAGdX9nWtOtK4OuZ-NLfPXd8Cx3qiwBUOOY";
+        private const string TELEGRAM_TOKEN = "";
         private ITelegramBotClient bot;
         private static TelegramBot instance;
         public static TelegramBot Instance
